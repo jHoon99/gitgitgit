d25b432a8b2db708522ba57e2b139f9c0e2af2fa
jHoon99 <eldkf1597@naver.com>
2025-04-10 20:11:21 +0900

    Update ContentView.swift
    .font(title2)

--- git_test/git_test/ContentView.swift
+++ git_test/git_test/ContentView.swift
@@ -19,6 +19,7 @@ struct ContentView: View {
             
                 .foregroundStyle(.tint)
             Text("블랍리ㅡ빌비ㅏ라ㅣㅂ라ㅓㅂㄷ라ㅣㅓㅂㄷㄹ")
+                .font(title1)
         }
         .padding()
     }
